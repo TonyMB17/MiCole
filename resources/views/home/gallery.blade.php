05c48b8f10a09adc7284c9d59b80c3d55a6f3f0f
TonyMB17 <anthonyamb17@gmail.com>
2024-12-19 09:01:41 -0500

    add images on gallery

--- resources/views/home/gallery.blade.php
+++ resources/views/home/gallery.blade.php
@@ -18,20 +18,294 @@
     <div class="container">
         <div class="text-center pb-2">
             <p class="section-title px-5"><span class="px-2">Fotos</span></p>
-            <h1 class="mb-4">Galeria de Fotos</h1>
+            <h1 class="mb-4">La Ruta del Agua</h1>
         </div>
-        <div class="row">
+
+        <div class="pb-1">
+            <h4 class="mb-4">IIEE José Maria Arguedas <small>/Anco Huallo/Chincheros</small></h4>
+        </div>
+        <div class="row portfolio-container">
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/1.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/1.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/2.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/2.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/3.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/3.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/4.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/4.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/5.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/5.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/6.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/6.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="pb-1">
+            <h4 class="mb-4">IIEE Saywite <small>/Curahuasi/Abancay</small></h4>
+        </div>
+        <div class="row portfolio-container">
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/7.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/7.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/8.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/8.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/9.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/9.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/10.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/10.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="pb-1">
+            <h4 class="mb-4">IIEE Andres Avelino Cáceres de Huarapari <small>/Huaccana/Chincheros</small></h4>
+        </div>
+        <div class="row portfolio-container">
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/11.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/11.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/12.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/12.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/13.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/13.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/14.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/14.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/15.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/15.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/16.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/16.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="pb-1">
+            <h4 class="mb-4">IIEE San Juan Bautista de Callebamba <small>/Chincheros/Chincheros</small></h4>
+        </div>
+        <div class="row portfolio-container">
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/17.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/17.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/18.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/18.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/19.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/19.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/20.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/20.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/21.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/21.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
+                <div class="position-relative overflow-hidden mb-2">
+                    <img class="img-fluid w-100 portfolio-img" src="{{ asset('home/img/rutaAgua/22.jpeg') }}"
+                        alt="">
+                    <div class="portfolio-btn bg-primary-gallery d-flex align-items-center justify-content-center">
+                        <a href="{{ asset('home/img/rutaAgua/23.jpeg') }}" data-lightbox="portfolio">
+                            <i class="fa fa-plus text-white" style="font-size: 60px;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+    {{-- <div class="row">
             <div class="col-12 text-center mb-2">
                 <ul class="list-inline mb-4" id="portfolio-flters">
                     <li class="btn btn-outline-primary m-1 active" data-filter="*">Todas</li>
-                    {{-- <li class="btn btn-outline-primary m-1" data-filter=".first">Agua</li>
+                    <li class="btn btn-outline-primary m-1" data-filter=".first">Agua</li>
                     <li class="btn btn-outline-primary m-1" data-filter=".second">Drawing</li>
-                    <li class="btn btn-outline-primary m-1" data-filter=".third">Reading</li> --}}
+                    <li class="btn btn-outline-primary m-1" data-filter=".third">Reading</li>
                 </ul>
             </div>
         </div>
         <div class="row portfolio-container">
-            @foreach($images as $image)
+            @foreach ($images as $image)
             <div class="col-lg-4 col-md-6 mb-4 portfolio-item first">
                 <div class="position-relative overflow-hidden mb-2">
                     <img class="img-fluid w-100 portfolio-img" src="{{ asset($image->ruta) }}" alt="">
@@ -43,16 +317,16 @@
                 </div>
             </div>
         @endforeach
-        </div>
-    </div>
+        </div> --}}
+</div>
 </div>
 
 <style>
     .portfolio-img {
-    width: 100%;
-    height: 200px;
-    object-fit: cover;
-}
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+    }
 </style>
 <!-- Gallery End -->
 
