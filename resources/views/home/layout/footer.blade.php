9d31241d1e4169fc4920de99bb7108b8a8ca5de9
TonyMB17 <anthonyamb17@gmail.com>
2025-03-26 12:32:29 -0500

    add-personal-information

--- resources/views/home/layout/footer.blade.php
+++ resources/views/home/layout/footer.blade.php
@@ -67,7 +67,7 @@
                &copy; <a class="text-primary font-weight-bold" href="#">Gobierno Regional de Apurímac 2024</a>.
                Todos los derechos reservados. Designed
                by
-               <a class="text-primary font-weight-bold" href="">Desarrollo Social</a>
+               <a class="text-primary font-weight-bold" href="www.linkedin.com/in/anthony-meza-bautista-48801a323">Desarrollo Social - AMB</a>
            </p>
        </div>
    </div>
